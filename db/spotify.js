d11f63908b73a6bf3ae750c2107763ee09bda223
Evander Santana <evantually@Evanders-MacBook-Pro.local>
2020-09-24 22:55:01 -0400

    basics of the db is done!

---
+++ db/spotify.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const request = require('request')
+const querystring = require("querystring");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const spotifyApi = new SpotifyWebApi();
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const redirect = "http://localhost:8888/callback";
+
+
+
+router.get('/login', (req, res) => {
+    let scope = "";
+    res.redirect(
+        "https://accounts.spotify.com/authorize?" +
+        querystring.stringify({
+            response_type: "code",
+            client_id: clientId,
+            scope: scope,
+            redirect_uri: redirect,
+        }))
+})
+
+router.get('/callback', ((req, res) => {
+    let code = req.query.code || null;
+    let state = req.query.state || null;
+
+    let authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        form: {
+            code: code,
+            redirect_uri: redirect,
+            grant_type: "authorization_code"
+        },
+        headers: {
+            Authorization:
+                "Basic " +
+                new Buffer(clientId + ":" + clientSecret).toString("base64")
+        },
+        json: true
+    };
+    request.post(authOptions, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            let access_token = body.access_token,
+                refresh_token = body.refresh_token;
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+            // res.redirect('/home')
+            res.redirect('/home');
+        } else {
+            res.redirect(
+                "/#" +
+                querystring.stringify({
+                    error: "invalid_token"
+                })
+            );
+        }
+    });
+}))
+
+router.get("/home", (req, res) => {
+    res.send(
+        "This is the home screen. To search for an artist, add to the url above a forward slash and the name of the artist you wish to find! It should look like 'localhost:8000/home/drake'!"
+    );
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8888;
+
+//middleware
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.static("public"));
+app.use(express.json());
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/" + `Bopify`;
+
+// mongoose connection
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.once("open", () => {
+    console.log("connected to Spotify");
+});
+
+app.use("/", require("./db/spotify.js"));
+app.use("/search", require('./db/search.js'))
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
